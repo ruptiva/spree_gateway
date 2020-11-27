8e8b653e5db68575f39e0bea44dcf419dedd14c9
Jonathan Guarnieri <jonathan@ruptiva.com>
2020-11-27 15:37:09 -0300

    config Gateway::Recebee to return ActiveMerchant::Billing::RecebeeGateway

--- app/models/spree/gateway/recebee.rb
+++ app/models/spree/gateway/recebee.rb
@@ -2,10 +2,9 @@ module Spree
   class Gateway::Recebee < Gateway
     preference :login, :string
     preference :password, :string
-    preference :account, :string
 
     def provider_class
-      ActiveMerchant::Billing::SagePayGateway
+      ActiveMerchant::Billing::RecebeeGateway
     end
 
     def auto_capture?
